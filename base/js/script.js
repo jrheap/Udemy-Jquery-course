7e0bc21079e9356fc9ceab7a582c09edc213cb96
Jason Heap <jasonrheap@gmail.com>
2019-03-22 00:01:06 -0600

    playing with animations

--- base/js/script.js
+++ base/js/script.js
@@ -2,5 +2,24 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  // $(".red-box").fadeOut(2000);
+  // $(".red-box").fadeTo(1000, 0.2);
+  // $(".green-box").fadeTo(2000, 0.5);
+  // $(".blue-box").hide();
+  // $(".blue-box").slideToggle(2000)
+  // $("p").hide();
+  // $("p").slideDown(1000)
+  // $(".green-box").fadeToggle();
+  // $(".green-box").fadeToggle();
+
+
+
+  // ===== Custom functions  =====
+
+  $(".blue-box").animate({
+    "margin-left": "+=200px",
+  }, 1000, "linear")
+  
+  $(".blue-box").animate({
+    "margin-left": "-=200px",
+  }, 1000, "linear")
 });
